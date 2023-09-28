29158536cf31359e5c0f71e1f1991ecdab733b0c
fabalcu97 <fabalcu97@gmail.com>
2023-09-27 22:21:13 -0300

    refactore: Moved sections from components to page for better layout

--- src/routes/chat/+page.svelte
+++ src/routes/chat/+page.svelte
@@ -1,7 +1,44 @@
 <script lang="ts">
-  import Chat from '../../components/chat.svelte';
+  import { goto } from '$app/navigation';
+  import { clearSession, currentUser } from '$lib/pocketbase/auth';
+  import { createMessage } from '$lib/pocketbase/messages';
+  import ChatList from '../../components/chatList.svelte';
+  import Container from '../../components/container.svelte';
+
+  let newMessage = '';
+
+  function logout() {
+    clearSession();
+    goto('/');
+  }
+
+  function sendMessage() {
+    if (!newMessage) {
+      return;
+    }
+    createMessage(newMessage);
+    newMessage = '';
+  }
 </script>
 
-<div class="flex h-screen">
-  <Chat />
-</div>
+<Container>
+  <div class="flex flex-row justify-between pb-2">
+    <h2>Welcome {$currentUser?.username}</h2>
+    <button
+      class="flex-0 bg-red-200 rounded-md hover:bg-red-300 px-2"
+      on:click={logout}>Logout</button
+    >
+  </div>
+  <ChatList />
+  <form on:submit|preventDefault={sendMessage} class="basis-0 flex mt-5">
+    <input
+      bind:value={newMessage}
+      class="flex-1 border-2 rounded-full mr-3 pl-5"
+      placeholder="Enter your message"
+    />
+    <button
+      class="flex-2 bg-emerald-200 hover:bg-emerald-300 rounded-full px-5"
+      type="submit">Send</button
+    >
+  </form>
+</Container>
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -24,35 +24,36 @@
   }
 </script>
 
-<div class="flex h-screen">
-  <Container>
-    <h2 class="text-center my-5">Welcome!</h2>
-    <form on:submit|preventDefault={login} class="h-full flex flex-col p-10">
-      <input
-        required
-        bind:value={email}
-        class="mb-5 border-2 rounded-lg pl-5"
-        placeholder="Email"
-      />
-      <input
-        required
-        bind:value={password}
-        class="mb-5 border-2 rounded-lg pl-5"
-        placeholder="Password"
-        type="password"
-      />
-      {#if error}
-        <p class="text-xs text-red-700">* {error}</p>{/if}
-      <button
-        type="submit"
-        class="basis-0 rounded-full mb-5 py-1 bg-blue-200 hover:bg-blue-300"
-        >Login</button
-      >
-      <a
-        href="/sign-up"
-        class="basis-0 text-center rounded-full mb-5 py-1 bg-emerald-200 hover:bg-emerald-300"
-        >Sign Up</a
-      >
-    </form>
-  </Container>
-</div>
+<Container>
+  <form
+    on:submit|preventDefault={login}
+    class="h-full flex flex-col p-10 justify-center"
+  >
+    <h2 class="text-center">Welcome!</h2>
+    <input
+      required
+      bind:value={email}
+      class="my-5 border-2 rounded-lg pl-5"
+      placeholder="Email"
+    />
+    <input
+      required
+      bind:value={password}
+      class="mb-5 border-2 rounded-lg pl-5"
+      placeholder="Password"
+      type="password"
+    />
+    {#if error}
+      <p class="text-xs text-red-700">* {error}</p>{/if}
+    <button
+      type="submit"
+      class="basis-0 rounded-full mb-5 py-1 bg-emerald-200 hover:bg-emerald-300"
+      >Login</button
+    >
+    <a
+      href="/sign-up"
+      class="basis-0 text-center rounded-full mb-5 py-1 bg-blue-200 hover:bg-emerald-300"
+      >Sign Up</a
+    >
+  </form>
+</Container>
